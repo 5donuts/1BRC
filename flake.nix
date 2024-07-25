5e89d0c8ec51d87c7ce968bbce18828381d9eb5e
Charles German <5donuts@pm.me>
2024-07-25 11:09:21 -0400

    Initial pass at a Nix Flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+# 1BRC - my take on the 1 Billion Row Challenge
+# Copyright (C) 2024  Charles German <5donuts@pm.me>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+{
+  description = "My take on the 1 Billion Row Challenge";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+  let
+    # For details on this approach to supporting multiple architectures, see:
+    # https://xeiaso.net/blog/nix-flakes-1-2022-02-21/
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+    nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; } );
+
+    rustOverrides = (builtins.fromTOML (builtins.readFile ./rust-toolchain.toml));
+  in {
+    # For details, see: https://nixos.wiki/wiki/Rust#Installation_via_rustup
+    devShells = forAllSystems (system:
+      let
+        pkgs = nixpkgsFor.${system};
+      in {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            clang
+            llvmPackages_latest.bintools
+            rustup
+          ];
+
+          RUSTC_VERSION = rustOverrides.toolchain.channel;
+        };
+      });
+  };
+}
