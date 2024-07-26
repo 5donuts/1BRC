a0483cf8e4ca78abcadc0429dc1dd6d01d221dca
Charles German <5donuts@pm.me>
2024-07-26 17:08:41 -0400

    Initial skeleton for src/naive.rs

--- src/naive.rs
+++ src/naive.rs
@@ -13,15 +13,58 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+use std::fs::File;
+use std::io::Read;
 use std::path::Path;
-use std::time::Duration;
+use std::time::Instant;
+use std::collections::HashMap;
 
-use crate::ChallengeRunner;
+use crate::helpers::*;
+
+struct StationData {
+    min: f32,
+    max: f32,
+
+    // Rather than compute a new average at each step, just keep a rolling sum
+    // of all the measurements and calculate the average at the end.
+    sum: f32,
+    cnt: u32,
+}
+
+impl StationData {
+    fn avg(&self) -> f32 {
+        self.sum / self.cnt as f32
+    }
+}
 
 pub struct Runner;
 
 impl ChallengeRunner for Runner {
-    fn run(input: &Path) -> Duration {
-        todo!()
+    fn run(input: &Path) -> ChallengeResult {
+        let start = Instant::now();
+
+        // A quick 'wc -L measurements.txt' suggests the longest line will be in the
+        // realm of 45 characters. So, make the buffer long enough to hold a good number
+        // of lines to reduce the number of I/O operations.
+        const BUFFER_SIZE: usize = std::mem::size_of::<char>() * 50000;
+        let mut buffer = [0; BUFFER_SIZE];
+        let mut map: HashMap<String, StationData> = HashMap::new();
+
+        let mut f = File::open(input)?;
+
+        let mut n = f.read(&mut buffer[..])?;
+        while n > 0 {
+            // TODO: actually do something with the data lol
+            n = f.read(&mut buffer[..])?;
+        }
+
+        // Build the alphabetically-sorted list of stations
+        let stations = Vec::new(); // TODO
+
+        // Compute the time it took to generate the list of sorted stations
+        let stop = Instant::now();
+        let duration = stop.duration_since(start);
+
+        Ok((stations, duration))
     }
 }
